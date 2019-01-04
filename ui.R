04740c076a1e52b22af104cd4d197809653e89d6
Samuel2m <samuel.de-maleprade@edu.dsti.institute>
2019-01-04 01:01:01 +0100

    Add files via upload

--- ui.R
+++ ui.R
@@ -43,7 +43,14 @@ ui <- fluidPage(
                     "Alcohol",
                     min = 0,
                     max = 15,
-                    value = c(9.5, 11.1))
+                    value = c(9.5, 11.1)),
+        
+        h6("This application uses a data set from the UCI repository on red wine quality"),
+        h6("The above filters concern the variables which have the most impact of the quality"),
+        h6("The checkbox allow you to chose if you want to use the filter"),
+        h6("The sliders to select the ranges on the variables, by default between the first and third quartile"),
+        h6("The filters apply to all the tabs (one tab per variable selected)"),
+        h6("I did not implement a predictive model, as just applying the filters dynamically needs reactive expressions, main topic of the project")
         ),
 
         
